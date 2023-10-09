e5c1f4649acc7463899145ee776c3a3dbc127162
Timofey Mukhin <muhin@glos-trust.com>
2023-10-10 06:03:46 +0700

    install tailwind and telescope. Also add rate limit.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -24,8 +25,16 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        RateLimiter::for('global', function (Request $request) {
+            return Limit::perMinute(500)
+                ->by($request->user()?->id ?: $request->ip());
+        });
+
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip())
+                ->response(function (Request $request, array $headers){
+                    return response('Take it easy', Response::HTTP_TOO_MANY_REQUESTS, $headers);
+                });
         });
 
         $this->routes(function () {
