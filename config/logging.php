eb3d069327a5af24f6d11d94428ccb923d606230
Timofey Mukhin <muhin@glos-trust.com>
2023-10-11 04:28:15 +0700

    telegram logger, console command added

--- config/logging.php
+++ config/logging.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Logging\Telegram\TelegramLoggerFactory;
 use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
@@ -125,6 +126,14 @@ return [
 
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
+        ],
+
+        'telegram' => [
+            'driver' => 'custom',
+            'via' => TelegramLoggerFactory::class,
+            'level' => env('LOG_LEVEL', 'debug'),
+            'chat_id' => '-4062340054',
+            'token' => '6499869157:AAHD04Xq9Jt0YmSjvb4rNIwBoftL43J-Ehs',
         ],
     ],
 
