71cf32942857cd3060f945bc96b2d5695f11b11a
Timofey Mukhin <muhin@glos-trust.com>
2023-10-15 12:29:37 +0700

    homework complete

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -2,17 +2,33 @@
 
 namespace App\Services\Telegram;
 
+use App\Exceptions\TelegramApiException;
 use Illuminate\Support\Facades\Http;
 
 class TelegramBotApi
 {
 
     public const HOST = 'https://api.telegram.org/bot';
-    public static function sendMessage(string $token, int $chatId, string $text): void
+    public static function sendMessage(string $token, int $chatId, string $text): bool
     {
-        Http::get(self::HOST . $token . '/sendMessage', [
-            'chat_id' => $chatId,
-            'text' => $text
-        ]);
+        try {
+            $response = Http::get(self::HOST . $token . '/sendMessage', [
+                'chat_id' => $chatId,
+                'text' => $text
+            ]);
+
+            if ($response->successful()) {
+                $responseData = $response->json();
+                if (isset($responseData['ok']) && $responseData['ok'] === true) {
+                    return true;
+                } else {
+                    throw new TelegramApiException('Telegram API returned an error: ' . json_encode($responseData));
+                }
+            } else {
+                throw new TelegramApiException('Telegram API request was not successful.');
+            }
+        } catch (\Exception $e) {
+            throw new TelegramApiException('Failed to send message to Telegram: ' . $e->getMessage(), 0, $e);
+        }
     }
 }
